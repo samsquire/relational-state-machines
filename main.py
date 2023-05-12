c4f7cd1639f0e418cf4126beacb8aaaf17fe2c2f
samsquire <sam@samsquire.com>
2023-05-12 12:03:49 +0000

    Create hierarchy of statelines

--- main.py
+++ main.py
@@ -11,29 +11,59 @@ thread_free(next_free_thread) = fork(A, B)
                                  { await(A, B, returnvalue) | paused(A, 1) }
                                | send_returnvalue(B, A, returnvalue)   
                                """
+
+class State:
+  def __init__(self, state):
+    self.state = state
+    self.children = []
+    self.parents = []
+    self.indent = 0
+  def add_parent(self, state):
+    self.parents.append(state)
+  def add_state(self, state):
+    state.add_parent(self)
+    self.children.append(state)
+    state.indent = self.indent + 1
+  def __repr__(self):
+    data = (self.indent * "  ") + str(self.state) + "\n"
+    for child in self.children:
+      data += str(child)
+    return data
+      
 # how to generate sequence of mailboxes and therefore locks and concurrency (set as a key)
 # leveldb string matching
 states = [
   [["running_on", ["A", "thread1"]], ["thread", ["thread1"]], ["assignment", ["A", "t"]]],
   [["send_task_to_thread", ["B", "next_free_thread"]]],
-  [["running_on", ["B", 2]], ["paused", ["B", 1]], ["running_on", ["A", 1] ]]
+  [["running_on", ["B", "2"]], ["paused", ["B", "1"]], ["running_on", ["A", 1] ]]
 ]
 
 def create_mailboxes(states):
   mailboxes = []
   history = ""
+  depth = 0
+  parent = State("root")
   for state in states:
-    
+    stateline = State("stateline{}".format(depth))
+    depth = depth + 1
+                                          
+    parent.add_state(stateline)
     for item in state:
-      
+      child = State(item[0])
+      stateline.add_state(child)
       history = "{} {}".format(history, item[0])
       # mailboxes.append(history)
       
       for subitem in item[1]:
+        subchild = State(subitem)
+        child.add_state(subchild)
         history = "{}_{}".format(history, subitem)
         mailboxes.append(history)
-  return mailboxes
+  return mailboxes, parent
 
-mb = create_mailboxes(states)
+mb, state = create_mailboxes(states)
 from pprint import pprint
 pprint(mb)
+pprint(state)
+
+# use ^ hat symbol to refer to parent
